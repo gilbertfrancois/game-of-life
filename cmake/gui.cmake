0355ac8cddcd296fc016fd6224d25aa8fc112cfb
Gilbert François <info@gilbertfrancois.com>
2023-01-28 00:12:45 +0100

    Removes commented line

--- cmake/gui.cmake
+++ cmake/gui.cmake
@@ -1,5 +1,7 @@
 find_package(SDL2 REQUIRED)
 include_directories(${SDL2_INCLUDE_DIRS})
+message(STATUS ${SDL2_INCLUDE_DIRS})
+message(STATUS ${SDL2_LIBRARIES})
 
 add_executable(game-of-life-gui
     src/gui/main.cpp
@@ -9,4 +11,3 @@ add_executable(game-of-life-gui
 
 # Link SDL2::Main, SDL2::Image and SDL2::GFX to our project
 target_link_libraries(game-of-life-gui ${SDL2_LIBRARIES})
-# target_link_libraries(game-of-life-gui SDL2::SDL2 SDL2::SDL2main)
